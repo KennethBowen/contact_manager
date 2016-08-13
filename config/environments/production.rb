bbb2fcf3fc266a874c908b999ce78a37869fac4a
Kenneth Bowen <bowenk479@gmail.com>
2016-08-12 17:34:36 -0700

    fixed configuration deprication in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # heroku configurement
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
 end
