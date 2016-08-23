1f0d63f2ba0d70f78d51e773395f6e72c29f8ada
Kenneth Bowen <bowenk479@gmail.com>
2016-08-22 18:34:27 -0700

    implemented delete function for phone numbers

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -45,4 +45,10 @@ describe 'the person view', type: :feature do
     expect(page).to_not have_content(old_number)
   end
 
+  it "has a link to delete a phone number" do
+    person.phone_numbers.each do |phone|
+      expect(page).to have_link('delete')
+    end
+  end
+
 end
