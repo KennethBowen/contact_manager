3b3c40ba490a90e6a9a89be5c662aef1146aebd6
Kenneth Bowen <bowenk479@gmail.com>
2016-08-26 15:50:33 -0700

    created email address relationship

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813003636) do
+ActiveRecord::Schema.define(version: 20160826224738) do
+
+  create_table "email_addresses", force: :cascade do |t|
+    t.string   "address"
+    t.integer  "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
